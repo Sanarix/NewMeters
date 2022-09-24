9da4425c97a2653525952c5ef5b65df34b0d85d7
Leonid Korsakov <KorsakovLA@mail.ru>
2022-09-24 19:38:47 +0300

    примерка теста

---
+++ js/main.js
@@ -0,0 +1,28 @@
+'use strict'
+const overlay = document.querySelector('.overlay');
+const testField = document.querySelector('.test');
+const startButton = document.querySelector('.start-button');
+
+class Test {
+	constructor() {
+		this.type = null;
+		this.apartment = null;
+		this.contribution = null;
+	}
+
+	start() {
+		textField.innerHTML = `
+		
+		`
+	}
+}
+
+startButton.addEventListener('click', () => {
+	overlay.classList.remove('hide');
+	let test = new Test();
+	test.start();
+});
+
+function startTest() {
+
+}
